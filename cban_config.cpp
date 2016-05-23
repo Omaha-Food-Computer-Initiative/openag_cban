e78251c9a6af58a2f7e45cdd1f066be4bd2255ee
Jake Rye <jrye@mit.edu>
2016-05-23 12:14:13 -0400

    added support for ds18b20

--- cban_config.cpp
+++ cban_config.cpp
@@ -1,13 +1,16 @@
 #include "cban_handler.h"
 
 #include <openag_am2315.h>
-Am2315 am2315_1("am2315_1"), am2315_2("am2315_2");
+String am2315_1_parameters[] = {};
+Am2315 am2315_1("am2315_1", am2315_1_parameters);
 
-
+#include <openag_ds18b20.h>
+String ds18b20_1_parameters[] = {"22"};
+Ds18b20 ds18b20_1("ds18b20_1", ds18b20_1_parameters);
 
 CbanHandler::CbanHandler() {
 	_num_peripherals = 2;
 	_peripheral_array = new Peripheral*[2];
 	_peripheral_array[0] = &am2315_1;
-	_peripheral_array[1] = &am2315_2;
+	_peripheral_array[1] = &ds18b20_1;
 }
