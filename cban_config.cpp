4913cc4790d7eb116a21a4b9cd8ddc433d3ceb75
Jake Rye <jrye@mit.edu>
2016-05-23 14:12:58 -0400

    added support for atlas ph

--- cban_config.cpp
+++ cban_config.cpp
@@ -8,9 +8,14 @@ Am2315 am2315_1("am2315_1", am2315_1_parameters);
 String ds18b20_1_parameters[] = {"22"};
 Ds18b20 ds18b20_1("ds18b20_1", ds18b20_1_parameters);
 
+#include <openag_atlas_ph.h>
+String atlas_ph_1_parameters[] = {"99"};
+AtlasPh atlas_ph_1("atlas_ph_1", atlas_ph_1_parameters);
+
 CbanHandler::CbanHandler() {
-	_num_peripherals = 2;
+	_num_peripherals = 3;
 	_peripheral_array = new Peripheral*[2];
 	_peripheral_array[0] = &am2315_1;
 	_peripheral_array[1] = &ds18b20_1;
+	_peripheral_array[2] = &atlas_ph_1;
 }
